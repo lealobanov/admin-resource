1d237974bcb010d8736b9cca83ef3042a6058e6d
lealobanov <lea.lobanov@icloud.com>
2023-10-13 19:58:29 +0900

    Remove description

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
 export const adminResource= {
   slug: recipe,
   title: "Admin Resource",
-  description: "https://play.onflow.org/a7d190b6-e0f1-4acc-b34c-f37b39fbab33?type=tx&id=c252ea40-397c-43b0-acfb-c504a7268175&storage=none",
   createdAt: Date(2022, 3, 1),
   author: "Flow Blockchain",
   playgroundLink:
-    "",
+  "https://play.onflow.org/a7d190b6-e0f1-4acc-b34c-f37b39fbab33?type=tx&id=c252ea40-397c-43b0-acfb-c504a7268175&storage=none",
   excerpt:
     "Using an admin resource to have exclusive access to functions in a smart contract.",
   smartContractCode: contractPath,
