8f32cac67d7a36a2d1f5d6c8d7c879a560e60a1c
lealobanov <lea.lobanov@icloud.com>
2023-10-17 21:13:05 +0900

    Debugging dates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
 export const adminResource= {
   slug: recipe,
   title: "Admin Resource",
-  createdAt: Date(2022, 3, 1),
+  createdAt: new Date(2022, 3, 1),
   author: "Flow Blockchain",
   playgroundLink:
   "https://play.onflow.org/a7d190b6-e0f1-4acc-b34c-f37b39fbab33?type=tx&id=c252ea40-397c-43b0-acfb-c504a7268175&storage=none",
